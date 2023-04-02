da747b7b3f196b02a5b0c824e79c7a5cf25d5352
Stanislav-Zhuk <steys.ua@gmail.com>
2023-04-02 15:13:23 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { renderGallery } from './js/render-gallery';
+import { PixabayApi } from './js/pixabay-api';
 
+// params library notiflix
 Notify.init({
   borderRadius: '10px',
   timeout: 4000,
   clickToClose: true,
   cssAnimationStyle: 'zoom',
 });
+
+// use library SimpleLightbox
+const SLBparams = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox('.gallery a', SLBparams);
+
+// declaration of variables and elements of DOM
+const pixabayApi = new PixabayApi();
+
+const gallery = document.querySelector('.gallery');
+const form = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');

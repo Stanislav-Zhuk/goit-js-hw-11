9f00e6a8c940ea8287a8f5e3bff85fd14dca3b1d
Stanislav-Zhuk <steys.ua@gmail.com>
2023-04-02 14:31:58 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,18 +4,19 @@ export class PixabayApi {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '34785717-8063b5203a171717f86304ea0';
 
-  options = new URLSearchParams([
+  params = new URLSearchParams([
     ['image_type', 'photo'],
     ['orientation', 'horizontal'],
     ['safesearch', 'true'],
     ['per_page', 40],
   ]);
+  searchQuery = '';
   page = 1;
 
-  async fetchImages(query) {
+  async fetchImages() {
     try {
       const response = await axios(
-      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.query}&page=${this.page}&${this.options}`
+      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.searchQuery}&page=${this.page}&${this.params}`
     );
 
     return await response.data;
